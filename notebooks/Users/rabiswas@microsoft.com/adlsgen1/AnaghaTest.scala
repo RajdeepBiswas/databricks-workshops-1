6783d6b9ac0ee1b15b226958ee5f968b9fb4cef1
Rajdeep Biswas <clickrajdeep@gmail.com>
2019-03-18 13:51:01 -0500

    Update: README.md data-engineering data-science general-workshop iot nosql notebooks

---
+++ notebooks/Users/rabiswas@microsoft.com/adlsgen1/AnaghaTest.scala
@@ -0,0 +1,19 @@
+// Databricks notebook source
+display(dbutils.fs.ls("/mnt/adlsgen1/summary4-data"))
+
+// COMMAND ----------
+
+// MAGIC %fs
+// MAGIC cp /mnt/adlsgen1/summary4-data/If-By-Kipling.txt /mnt/adlsgen1/summary4-data/If-By-Kipling2.txt 
+
+// COMMAND ----------
+
+display(dbutils.fs.ls("/mnt/adlsgen1/summary4-data"))
+
+// COMMAND ----------
+
+// MAGIC %fs
+// MAGIC cp /mnt/adlsgen1/summary4-data/If-By-Kipling.txt /mnt/adlsgen1/summary4-data/If-By-Kipling33.txt 
+
+// COMMAND ----------
+
